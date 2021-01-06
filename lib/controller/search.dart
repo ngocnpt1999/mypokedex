b72099d1ee1ad3274caedea10edfe398f4cd2079
ngocnpt1999 <ngocnpt1999@gmail.com>
2021-01-07 01:27:42 +0700

    Add search history

--- lib/controller/search.dart
+++ lib/controller/search.dart
@@ -4,9 +4,15 @@ import 'package:mypokedex/pokemon_detail.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SearchPokemon extends SearchDelegate {
-  SearchPokemon(this.prefs);
+  SearchPokemon(this._prefs) {
+    if (_prefs.containsKey("recentSearchPokemon")) {
+      _recents = _prefs.getStringList("recentSearchPokemon");
+    }
+  }
 
-  final SharedPreferences prefs;
+  final SharedPreferences _prefs;
+
+  List<String> _recents = List();
 
   @override
   List<Widget> buildActions(BuildContext context) {
@@ -24,7 +30,8 @@ class SearchPokemon extends SearchDelegate {
   Widget buildLeading(BuildContext context) {
     return IconButton(
       icon: Icon(Icons.arrow_back),
-      onPressed: () {
+      onPressed: () async {
+        await _prefs.setStringList("recentSearchPokemon", _recents);
         Navigator.pop(context);
       },
     );
@@ -42,10 +49,21 @@ class SearchPokemon extends SearchDelegate {
     return ListView.separated(
       itemCount: pokeNames.length,
       itemBuilder: (context, index) => ListTile(
-        onTap: () {
+        onTap: () async {
+          if (!_recents.contains(pokeNames[index])) {
+            _recents.insert(0, pokeNames[index]);
+            if (_recents.length > 20) {
+              _recents.removeLast();
+            }
+          } else {
+            _recents.remove(pokeNames[index]);
+            _recents.insert(0, pokeNames[index]);
+          }
+          await _prefs.setStringList("recentSearchPokemon", _recents);
           Navigator.pop(context);
           Get.to(PokemonDetailPage(name: pokeNames[index]));
         },
+        leading: query.isEmpty ? Icon(Icons.history_rounded) : null,
         title: Text(
           pokeNames[index][0].toUpperCase() + pokeNames[index].substring(1),
         ),
@@ -56,8 +74,9 @@ class SearchPokemon extends SearchDelegate {
 
   void _getSuggestions(List<String> pokeNames) {
     if (query.isEmpty) {
+      pokeNames.addAll(_recents);
     } else {
-      pokeNames.addAll(prefs
+      pokeNames.addAll(_prefs
           .getStringList("pokedex")
           .where((e) => e.startsWith(query.toLowerCase()))
           .take(20));
