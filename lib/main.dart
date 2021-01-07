f9c9f4444db6f0f9905c6111f10d8bfb4beea7fc
ngocnpt1999 <ngocnpt1999@gmail.com>
2021-01-07 21:59:05 +0700

    Change app title

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Flutter Demo',
+      title: 'MyPokedex',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
