56fdfd6fcaa7bb6e6004db492cf12c71d844b6b9
ngocnpt1999 <ngocnpt1999@gmail.com>
2021-02-28 02:34:29 +0700

    Fix pokemon name text

--- lib/extension/stringx.dart
+++ lib/extension/stringx.dart
@@ -11,7 +11,7 @@ extension StringX on String {
     } else if (sub == "f") {
       sub = "♀";
       newStr = newStr.substring(0, index) + " " + sub;
-    } else if (sub == "mime" || sub == "jr") {
+    } else if (sub == "jr" || sub == "mime" || sub == "rime") {
     } else if (sub.isNotEmpty) {
       newStr = sub + " " + newStr.substring(0, index);
     }
